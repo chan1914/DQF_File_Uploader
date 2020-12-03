335c0594708c5a93004ba2a698ab5f7f0b1d56fd
asbj2903 <asbj2903@edu.ucl.dk>
2020-12-03 15:22:01 +0100

    working on passing the test, currently massive stack errors and the message "source code does not match bytecode"

--- src/main/java/nets/CSV/webApplication/CSVDigester/CSVDigester.java
+++ src/main/java/nets/CSV/webApplication/CSVDigester/CSVDigester.java
@@ -1,6 +1,8 @@
 package nets.CSV.webApplication.CSVDigester;
 
+import net.minidev.json.JSONObject;
 import org.springframework.stereotype.Service;
+
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -10,18 +12,18 @@ import java.util.ArrayList;
 @Service
 public class CSVDigester {
 
-    public void CSVtoJSON(String file){
+    public void CSVToJSON(String file){
 
         //Input file which needs to be parsed
         String fileToParse = "SampleCSVFile.csv";
         BufferedReader fileReader = null;
+        ArrayList<String> lines = new ArrayList<String>();
 
         //Delimiter used in CSV file
         final String DELIMITER = ",";
         try
         {
             String line = "";
-            ArrayList<String> lines = new ArrayList<String>();
             //Create the file reader
             fileReader = new BufferedReader(new FileReader(fileToParse));
 
@@ -45,14 +47,94 @@ public class CSVDigester {
                 e.printStackTrace();
             }
         }
+        generateJSON(lines);
 
     }
 
-    private String FindType(String line){
+    public void generateJSON(ArrayList<String> lines){
 
+        for (int i = 0; i < lines.size(); i++) {
+            JSONObject lineJSON = new JSONObject();
+            if (i > 0){
 
-        return "";
+                int iterator = 0;
+                for (String str : lines.get(i).split(",")) {
+
+                    if (typeSelector(str) == "string"){
+                        lineJSON.put(String.valueOf(lines.get(0).split(",")[iterator]), String.valueOf(str));
+                    }
+                    else {
+                        lineJSON.put(String.valueOf(lines.get(0).split(",")[iterator]), str);
+                    }
+
+                    }
+                iterator ++;
+                }
+            }
+        }
+
+    private String typeSelector( String element){
+
+        String result;
+        result = checkDouble(element);
+        if (!(result == "N/A")){
+            return result;
+        }
+        result = checkInt(element);
+        if (!(result == "N/A")){
+            return result;
+        }
+        result = checkBoolean(element);
+        if (!(result == "N/A")){
+            return result;
+        }
+        return "string";
     }
+
+    private String checkDouble(String element){
+        String result;
+
+        try {
+            Double.parseDouble(element);
+            result = "double";
+        }
+        catch (NumberFormatException e) {
+            result = "N/A";
+        }
+        return result;
+    }
+
+    private String checkInt(String element){
+        String result;
+
+        try {
+            Integer.parseInt(element);
+            result = "int";
+        }
+        catch (NumberFormatException e) {
+            result = "N/A";
+        }
+        return result;
+    }
+
+    private String checkBoolean(String element){
+        String result;
+
+        if (element.toLowerCase() == "false"){
+            result = "boolean";
+        }
+        else if (element.toLowerCase() == "true"){
+            result = "boolean";
+        }
+        else{
+            result = "N/A";
+        }
+        return result;
+    }
+
+
+
+
 
 
     public String SerializeRow(String headers, String row) {
