9e6f2879817ddc966452d57b213985bffb11bec3
Mikkel Jørgensen <mikk149c@edu.eal.dk>
2020-12-22 14:02:43 +0100

    Async

--- src/main/java/nets/CSV/webApplication/WebApplication.java
+++ src/main/java/nets/CSV/webApplication/WebApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.http.HttpHeaders;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableAsync
 public class WebApplication{
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
