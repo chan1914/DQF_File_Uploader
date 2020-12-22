7db18d2887c7522f3841346da940dfe0da2744c1
Mikkel Jørgensen <mikk149c@edu.eal.dk>
2020-12-22 14:13:08 +0100

    Async

--- src/main/java/nets/CSV/webApplication/WebApplication.java
+++ src/main/java/nets/CSV/webApplication/WebApplication.java
@@ -6,9 +6,12 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.http.HttpHeaders;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -24,5 +27,14 @@ public class WebApplication{
 		return new RestTemplate();
 	}
 
+	@Bean (name = "taskExecutor")
+	public Executor taskExecutor(){
+		final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(2);
+		executor.setMaxPoolSize(32);
+		executor.setQueueCapacity(100);
+		executor.setThreadNamePrefix("FileLoaderThread-");
+		return executor;
+	}
 
 }
