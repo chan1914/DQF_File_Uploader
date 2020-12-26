f062afa802e88cd3a895271c883d9b6ff24269ec
mikk149c <mikk149c@edu.ucl.dk>
2020-12-26 16:39:11 +0100

    Added file delete button and hooked it up to delete the local files

--- src/main/java/nets/CSV/webApplication/Controller/DownloadFileController.java
+++ src/main/java/nets/CSV/webApplication/Controller/DownloadFileController.java
@@ -8,10 +8,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 
 import java.util.List;
@@ -56,9 +60,11 @@ public class DownloadFileController {
 					.body(file);	
 	}
 
-	@RequestMapping(value = "/delete/{fileName}", method = RequestMethod.POST)
-	public void deleteFile(@PathVariable("fileName") String fileName){
+	@DeleteMapping ("/delete/{fileName}")
+	public ResponseEntity<String> deleteFile(@PathVariable("fileName") String fileName){
 		logger.info("Deleting: " + fileName);
 		fileStorage.tryDeleteFile(fileName);
+
+		return new ResponseEntity<String>(fileName, HttpStatus.OK);
 	}
 }
